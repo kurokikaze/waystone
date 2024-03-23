deff4d4023e00cfaa717016a18648c5b169585b1
Sergei Shirokov <kurokikaze@yandex.ru>
2024-03-23 15:59:55 +0100

    Removed unused code

--- src/containedEngine/containedEngine.ts
+++ src/containedEngine/containedEngine.ts
@@ -21,22 +21,6 @@ export const createGame = (): State => {
     spellMetaData: {},
   };
 
-  const createZones = (player1: number, player2: number) => [
-    new Zone('Player 1 hand', ZONE_TYPE_HAND, player1),
-    new Zone('Player 2 hand', ZONE_TYPE_HAND, player2),
-    new Zone('Player 1 deck', ZONE_TYPE_DECK, player1),
-    new Zone('Player 2 deck', ZONE_TYPE_DECK, player2),
-    new Zone('Player 1 discard', ZONE_TYPE_DISCARD, player1),
-    new Zone('Player 2 discard', ZONE_TYPE_DISCARD, player2),
-    new Zone('Player 1 active magi', ZONE_TYPE_ACTIVE_MAGI, player1),
-    new Zone('Player 2 active magi', ZONE_TYPE_ACTIVE_MAGI, player2),
-    new Zone('Player 1 Magi pile', ZONE_TYPE_MAGI_PILE, player1),
-    new Zone('Player 2 Magi pile', ZONE_TYPE_MAGI_PILE, player2),
-    new Zone('Player 1 defeated Magi', ZONE_TYPE_DEFEATED_MAGI, player1),
-    new Zone('Player 2 defeated Magi', ZONE_TYPE_DEFEATED_MAGI, player2),
-    new Zone('In play', ZONE_TYPE_IN_PLAY, null),
-  ];
-
   const zones: any[] = []; //createZones(1, 2)
   const game = new State({
     ...defaultState,
