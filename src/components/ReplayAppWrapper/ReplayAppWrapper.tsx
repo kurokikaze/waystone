69a7b6333e832e4c390da61b0772ecf16cf1c930
Serge Shirokov <kurokikaze@yandex.ru>
2024-11-17 03:02:21 +0100

    A bit better interface for the replays

--- src/components/ReplayAppWrapper/ReplayAppWrapper.tsx
+++ src/components/ReplayAppWrapper/ReplayAppWrapper.tsx
@@ -1,14 +1,14 @@
 import { getCurrentWebviewWindow } from "@tauri-apps/api/webviewWindow";
 import { isTauri } from "@tauri-apps/api/core";
 import { LogicalSize } from "@tauri-apps/api/dpi";
-import { ACTION_PLAYER_WINS } from "moonlands";
+import { ACTION_EFFECT, ACTION_PLAYER_WINS, EFFECT_TYPE_START_OF_TURN } from "moonlands";
 import { useRef, useEffect, useState } from "react";
 import { Provider } from "react-redux"
 import { createStore, compose, applyMiddleware } from "redux";
 import { createEpicMiddleware } from "redux-observable";
 import thunk from "redux-thunk";
 import { Observable, Subscriber } from "rxjs";
-import Spin from "antd/es/spin";
+import { Spin, Slider, Button } from "antd";
 import addAnimations from "../../addAnimations";
 import { C2SAction, ClientAction, ClientMessage } from "../../clientProtocol";
 import { defaultState } from "../../reducers/reducer";
@@ -22,137 +22,163 @@ import './style.css';
 
 const epicMiddleware = createEpicMiddleware();
 const store = createStore(
-	rootReducer,
-	defaultState,
-	compose(
-		applyMiddleware(thunk),
-		applyMiddleware(epicMiddleware),
-	),
+    rootReducer,
+    defaultState,
+    compose(
+        applyMiddleware(thunk),
+        applyMiddleware(epicMiddleware),
+    ),
 );
 
 
 var emptyEngineConnector: EngineConnector = {
-	emit: (_action: C2SAction) => { },
+    emit: (_action: C2SAction) => { },
 };
 
 type ReplayAppWrapperProps = {
-	replayName: string
-	onReturnToBase: () => void
+    replayName: string
+    onReturnToBase: () => void
 }
 
 export const ReplayAppWrapper = ({
-	replayName,
-	onReturnToBase,
+    replayName,
+    onReturnToBase,
 }: ReplayAppWrapperProps) => {
-	const [engineConnector, setEngineConnector] = useState<EngineConnector>(emptyEngineConnector);
+    const [engineConnector, setEngineConnector] = useState<EngineConnector>(emptyEngineConnector);
 
-	const [loading, setLoading] = useState(true);
-	const [playing, setPlaying] = useState(true);
-	const replayDataSaved = useRef<ClientMessage[]>()
-	const actionSubscriber = useRef<Subscriber<ClientAction>>()
+    const [loading, setLoading] = useState(true);
+    const [playing, setPlaying] = useState(true);
+    const replayDataSaved = useRef<ClientMessage[]>()
+    const replayDataInitial = useRef<ClientMessage[]>()
+    const actionSubscriber = useRef<Subscriber<ClientAction>>()
+    const [ticks, setTicks] = useState<Record<number, string>>({})
 
-	const startPlayback = (speed = 100) => {
-		if (replayDataSaved.current && actionSubscriber.current) {
-			saveInterval(setInterval(() => {
-				if (!replayDataSaved.current) {
-					throw new Error('Replay stopped prematurely');
-				}
-				const messageData = replayDataSaved.current.shift();
-				if (messageData && 'state' in messageData) {
-					const data = messageData;
-					if (data.for === 1) {
-						console.log('Setting the initial state');
-						console.dir(messageData.state);
-						// @ts-ignore
-						store.dispatch({ type: 'setInitialState', state: enrichState(messageData.state, 1) });
-					}
-				} else if (messageData && 'action' in messageData) {
-					if (messageData.for === 1) {
-						actionSubscriber.current?.next(messageData.action);
-						if (messageData.action.type === ACTION_PLAYER_WINS) {
-							actionSubscriber.current?.complete();
-						}
-					}
-				}
-			}, speed));
-			setPlaying(true);
-		}
-	}
+    const startPlayback = (speed = 100) => {
+        if (replayDataSaved.current && actionSubscriber.current) {
+            saveInterval(setInterval(() => {
+                if (!replayDataSaved.current) {
+                    throw new Error('Replay stopped prematurely');
+                }
+                const messageData = replayDataSaved.current.shift();
+                if (messageData && 'state' in messageData) {
+                    const data = messageData;
+                    if (data.for === 1) {
+                        console.log('Setting the initial state');
+                        console.dir(messageData.state);
+                        // @ts-ignore
+                        store.dispatch({ type: 'setInitialState', state: enrichState(messageData.state, 1) });
+                    }
+                } else if (messageData && 'action' in messageData) {
+                    if (messageData.for === 1) {
+                        actionSubscriber.current?.next(messageData.action);
+                        if (messageData.action.type === ACTION_PLAYER_WINS) {
+                            actionSubscriber.current?.complete();
+                        }
+                    }
+                }
+            }, speed));
+            setPlaying(true);
+        }
+    }
 
-	const pause = () => {
-		setPlaying(false);
-		clearInterval(interval);
-	}
+    const pause = () => {
+        setPlaying(false);
+        clearInterval(interval);
+    }
 
-	const play = () => {
-		startPlayback();
-	}
+    const play = () => {
+        startPlayback();
+    }
 
-	const [interval, saveInterval] = useState<ReturnType<typeof setInterval>>()
-	const breakRef = useRef<Function>(() => { });
+    const rewind = () => {
+        if (replayDataInitial.current) {
+            replayDataSaved.current = [...replayDataInitial.current]
+            setCurrentPosition(0)
+        }
+    }
 
-	const loadReplay = async (replayName: string) => {
-		const replayData = await (new ReplayLogService()).readReplay(replayName);
-		replayDataSaved.current = replayData;
+    const [interval, saveInterval] = useState<ReturnType<typeof setInterval>>()
+    const breakRef = useRef<Function>(() => { });
+    const [length, setLength] = useState<number>(0)
+    const [currentPosition, setCurrentPosition] = useState<number>(0)
 
-		const actionsObservable = new Observable<ClientAction>(subscriber => {
-			actionSubscriber.current = subscriber;
+    const loadReplay = async (replayName: string) => {
+        const replayData = await (new ReplayLogService()).readReplay(replayName);
+        replayDataSaved.current = replayData;
+        replayDataInitial.current = [...replayData];
+        const ticksFromReplay = replayData
+            // @ts-ignore effectType is not always on ClientAction (?)
+            .map(({ action: { type = '', effectType = '' } = {}}, index) => [index, type, effectType])
+            .filter(([_, type, effectType]) => type == ACTION_EFFECT && effectType == EFFECT_TYPE_START_OF_TURN)
+            .map(([step], index) => [step, (index + 1).toString()])
+        setTicks(Object.fromEntries(ticksFromReplay))
+        setLength(replayData.length)
 
-			startPlayback();
-		});
+        const actionsObservable = new Observable<ClientAction>(subscriber => {
+            actionSubscriber.current = subscriber;
 
-		const breakObservable = new Observable<{}>(observer => {
-			breakRef.current = () => {
-				observer.next({});
-			}
-		});
-		const animatedActions = addAnimations(actionsObservable, breakObservable, store, false);
-		animatedActions.subscribe({
-			next: (transformedAction) => {
-				store.dispatch(transformedAction);
-			},
-		});
-		setLoading(false);
-	}
+            startPlayback();
+        });
 
-	useEffect(() => {
-		try {
-			if (isTauri()) {
-				const appWindow = getCurrentWebviewWindow()
+        const breakObservable = new Observable<{}>(observer => {
+            breakRef.current = () => {
+                observer.next({});
+            }
+        });
+        const animatedActions = addAnimations(actionsObservable, breakObservable, store, false);
+        animatedActions.subscribe({
+            next: (transformedAction) => {
+                setCurrentPosition(pos => pos + 1);
+                store.dispatch(transformedAction);
+            },
+        });
+        setLoading(false);
+    }
 
-				appWindow.setResizable(false);
-				appWindow.setSize(new LogicalSize(1111, 660));
-			}
-		} catch (e) {
+    useEffect(() => {
+        try {
+            if (isTauri()) {
+                const appWindow = getCurrentWebviewWindow()
 
-		}
-		const realEngineConnector = {
-			emit: (action: any) => () => {
-				switch (action.type) {
-					case 'actions/pause': {
-						clearInterval(interval);
-						setPlaying(false);
-						break;
-					}
-					case 'actions/play': {
-						startPlayback();
-						break;
-					}
-				}
-			}
-		};
-		setEngineConnector(realEngineConnector);
-		loadReplay(replayName);
-	}, [replayName]);
+                appWindow.setResizable(false);
+                appWindow.setSize(new LogicalSize(1111, 660));
+            }
+        } catch (e) {
 
-	return (
-		<div className="replayWrapper">
-			<Provider store={store}>
-				{loading ? <Spin /> : <GameApp engineConnector={engineConnector} onBreak={breakRef.current} onReturnToBase={onReturnToBase} playerId={1} />}
-				<div className="controls">
-					{playing ? <button onClick={pause}>Pause</button>
-						: <button onClick={play}>Play</button>}
-				</div>
-			</Provider>
-		</div>)
+        }
+        const realEngineConnector = {
+            emit: (action: any) => () => {
+                switch (action.type) {
+                    case 'actions/pause': {
+                        clearInterval(interval);
+                        setPlaying(false);
+                        break;
+                    }
+                    case 'actions/play': {
+                        startPlayback();
+                        break;
+                    }
+                }
+            }
+        };
+        setEngineConnector(realEngineConnector);
+        loadReplay(replayName);
+    }, [replayName]);
+
+    return (
+        <div className="replayWrapper">
+            <Provider store={store}>
+                {loading ? <Spin /> : <GameApp engineConnector={engineConnector} onBreak={breakRef.current} onReturnToBase={onReturnToBase} playerId={1} />}
+                <div className="controls">
+                    <div>
+                        <Slider disabled max={length} marks={ticks} value={currentPosition}></Slider>
+                    </div>
+                    <div className="playPause">
+                        <Button onClick={rewind}>Rewind</Button>
+                        {playing ? <Button onClick={pause}>Pause</Button>
+                            : <Button onClick={play}>Play</Button>}
+                    </div>
+                </div>
+            </Provider>
+        </div>)
 }
